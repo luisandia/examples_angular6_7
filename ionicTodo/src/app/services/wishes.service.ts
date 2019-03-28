1f7b8229df56dc16537c61311c65ccc8cfccda0a
Andial <luisandiamamani@gmail.com>
2019-03-27 23:29:16 -0500

    add localstorage

--- ionicTodo/src/app/services/wishes.service.ts
+++ ionicTodo/src/app/services/wishes.service.ts
@@ -9,14 +9,26 @@ export class WishesService {
   listas: Lista[] = [];
 
   constructor() {
-    const list1 = new Lista('recolect stones');
-    const list2 = new Lista('Heroes to dissapear');
-    this.listas.push(list1,list2);
-   }
+    this.loadStorage()
+    // const list1 = new Lista('recolect stones');
+    // const list2 = new Lista('Heroes to dissapear');
+    // this.listas.push(list1, list2);
+  }
 
-   createList(title:string){
+  createList(title: string) {
     const newList = new Lista(title);
     this.listas.push(newList);
+    this.saveStorage();
+  }
 
-   }
+  saveStorage() {
+    localStorage.setItem('data', JSON.stringify(this.listas));
+  }
+  loadStorage() {
+    if (localStorage.getItem('data')) {
+      this.listas = JSON.parse(localStorage.getItem('data'));
+    } else {
+      this.listas = [];
+    }
+  }
 }
